aab99dde6aafcdfc3002b291fd496f1b59287bfc
Ryosuke Ito <rito.0305@gmail.com>
2020-04-15 20:03:26 +0900

    Initial commit

---
+++ GraphQLCodeGenExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphQLCodeGenExample
+//
+//  Created by Ryosuke Ito on 4/15/20.
+//  Copyright © 2020 Ryosuke Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
